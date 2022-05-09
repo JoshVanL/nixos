1aaabca08fb0e0466c40ded3dedf9bc4d3136ad1
joshvanl <vleeuwenjoshua@gmail.com>
2022-05-09 23:56:29 +0100

    Update somestatus to v0.1.4
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- nixpkgs/somestatus/default.nix
+++ nixpkgs/somestatus/default.nix
@@ -12,13 +12,13 @@ in
 
 buildGo118Module rec {
   pname = "somestatus";
-  version = "0.1.2";
+  version = "0.1.4";
 
   src = fetchFromGitHub {
     owner = "joshvanl";
     repo = pname;
-    rev = "cfb1c55a71d6155a1c63630fb36acf9babd7171f";
-    hash = "sha256-4nbmPqdtGTWbiqIT7sTR9/SZeuEhYakQV8lm7Mt86eM=";
+    rev = "8e305144bd218bba44d362bddab6bf68b7992a66";
+    hash = "sha256-PY0bcT1Fa+Cry8OqY6Ln1Jn6vgC+P9AgyyjySHvjyJA=";
   };
 
   vendorSha256 = "sha256-jIrWIURv8od7NVoNvV4S63sIQRHmT4NfKIbaifawWQw=";
