00a5d98ea18c8c75b20d7099a33e4b035b4c1154
joshvanl <vleeuwenjoshua@gmail.com>
2022-05-12 17:04:39 +0100

    Adds another backup command help example
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- common/backup.nix
+++ common/backup.nix
@@ -51,6 +51,7 @@
   # $ zfs list -t snapshot
   # $ B2_APPLICATION_KEY_ID=xxx B2_APPLICATION_KEY=xxx backblaze-b2  download-file-by-name xxx rpool/safe/persist/xxxx.full persist.full
   # $ sudo zfs receive -F -v rpool/safe/restore < persist.full
+  # $ sudo zfs receive -F -v rpool/safe/restore < persist.inc
   # $ sudo zfs load-key -r rpool/safe/restore
   # $ sudo zfs set mountpoint=legacy rpool/safe/restore
   # $ sudo mount -t zfs rpool/safe/restore foo
