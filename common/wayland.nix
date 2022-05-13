5724d9736473afa21fe77d9f22cc51b603ff81c7
joshvanl <vleeuwenjoshua@gmail.com>
2022-05-13 11:14:55 +0100

    Set env vars for correct screen sharing
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- common/wayland.nix
+++ common/wayland.nix
@@ -11,4 +11,22 @@
   environment.systemPackages = with pkgs; [
     xwayland
   ];
+
+  # Screen sharing
+  xdg = {
+    portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal
+        xdg-desktop-portal-wlr
+      ];
+      wlr.enable = true;
+    };
+  };
+
+  environment.sessionVariables = rec {
+    XDG_SESSION_TYPE    = "wayland";
+    XDG_CURRENT_DESKTOP = "wayfire";
+    MOZ_ENABLE_WAYLAND  = "1";
+  };
 }
