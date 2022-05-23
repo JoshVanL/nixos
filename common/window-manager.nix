d94059d686d96375530b729cfcf88e5fe02f2404
joshvanl <vleeuwenjoshua@gmail.com>
2022-05-23 23:02:34 +0100

    Fix window-manager start.sh while loop _again_, lmao
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- common/window-manager.nix
+++ common/window-manager.nix
@@ -19,7 +19,10 @@
         dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=Wayfire
         systemctl --user stop pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-wlr
         systemctl --user start wireplumber
-        while somestatus; do; break; done
+        while somestatus
+        do
+          break
+        done
       '';
 
       mode = "755";
