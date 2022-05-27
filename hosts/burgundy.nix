21d79e048e55a778a8117ef78be09ea1999d29c1
joshvanl <vleeuwenjoshua@gmail.com>
2022-05-27 19:46:24 +0100

    Adds more pi options to burgundy
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- hosts/burgundy.nix
+++ hosts/burgundy.nix
@@ -16,6 +16,26 @@
   };
 
   boot = {
+    kernelPackages = pkgs.linuxPackages_rpi4;
+    initrd.availableKernelModules = [ "usbhid" "usb_storage" ];
+    # ttyAMA0 is the serial console broken out to the GPIO
+    kernelParams = [
+        "8250.nr_uarts=1"
+        "console=ttyAMA0,115200"
+        "console=tty1"
+        # A lot GUI programs need this, nearly all wayland applications
+        "cma=128M"
+    ];
+    loader.raspberryPi = {
+      enable = true;
+      version = 4;
+    };
+  };
+
+  # Required for the Wireless firmware
+  hardware.enableRedistributableFirmware = true;
+
+  boot = {
     initrd = {
       network = {
         enable = true;
