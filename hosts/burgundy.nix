155e0e3acc7bda543d33b68cac98ef70499582f5
joshvanl <vleeuwenjoshua@gmail.com>
2022-05-29 20:50:26 +0100

    ..
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- hosts/burgundy.nix
+++ hosts/burgundy.nix
@@ -68,12 +68,8 @@
 
   # VPN using tailscale (good software).
   services.tailscale.enable = true;
-  systemd.services.tailscaled = {
-    after = [ "systemd-tmpfiles-setup.service" ];
-    # Tailscale freaks out if /var/lib/tailscale is a sys link. Though it's
-    # probably systemd's fault.
-    serviceConfig.BindPaths = "/keep/var/lib/tailscale:/var/lib/tailscale";
-  };
+  systemd.services.tailscaled.after = [ "systemd-tmpfiles-setup.service" ];
+  fileSystems."/var/lib/tailscale" = { device = "/keep/var/lib/tailscale"; options = [ "bind" ]; };
   networking.firewall = {
     enable = true;
     trustedInterfaces = [ "tailscale0" ];
