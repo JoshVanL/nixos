71dc6eb86247c77e413ab69917e0589a0fb44526
joshvanl <vleeuwenjoshua@gmail.com>
2022-07-05 19:22:14 +0100

    Adds `.viminfo` as system link
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- modules/common/links.nix
+++ modules/common/links.nix
@@ -46,6 +46,7 @@
       "L+ /home/josh/downloads      - - - - /keep/home/downloads"
       "L+ /home/josh/documents      - - - - /persist/home/documents"
       "L+ /home/josh/.docker        - - - - /persist/home/.docker"
+      "L+ /home/josh/.viminfo       - - - - /persist/home/.viminfo"
 
       "L+ /home/josh/.config/chromium - - - - /persist/home/.config/chromium"
       "L+ /home/josh/.cache/chromium  - - - - /persist/home/.cache/chromium"
