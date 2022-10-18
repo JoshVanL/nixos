4ec2889b2ee93f82cc55fdc963e444d5738a7d64
joshvanl <me@joshvanl.dev>
2022-10-18 17:00:12 +0100

    Remove unused services from burgundy2
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- hosts/burgundy2.nix
+++ hosts/burgundy2.nix
@@ -10,16 +10,6 @@ let
     # gold-fido
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIFOiIRyDiW99jt4klbFBwWYaUDbt9x33vab+lummvaA2AAAABHNzaDo="
   ];
-  joshvanlPath = "/persist/etc/joshvanl";
-  joshvanlDNSPath = "${joshvanlPath}/dns";
-  dnsBitwarden = if builtins.pathExists "${joshvanlDNSPath}/domains/bitwarden"
-  then (builtins.readFile "${joshvanlDNSPath}/domains/bitwarden")
-    else "null"
-  ;
-  acmeEmail = if builtins.pathExists "${joshvanlDNSPath}/acme/email"
-    then (builtins.readFile "${joshvanlDNSPath}/acme/email")
-    else "null@null.com"
-  ;
 in {
   boot = {
     kernelPackages = pkgs.linuxPackages_rpi4;
@@ -94,110 +84,5 @@ in {
 
   security = {
     pam.yubico = { enable = true; id = "16"; };
-    acme = {
-      acceptTerms = true;
-      defaults = {
-        email = "${acmeEmail}";
-        dnsProvider = "gcloud";
-        credentialsFile = "${joshvanlDNSPath}/acme/credentials.secret";
-      };
-    };
-  };
-
-  services = {
-    openssh = {
-      enable = true;
-      passwordAuthentication = false;
-      kbdInteractiveAuthentication = true;
-    };
-    vaultwarden = {
-      enable = true;
-      dbBackend = "postgresql";
-      config = {
-        tz                 = "Europe/London";
-        domain             = "https://${dnsBitwarden}";
-        signupsAllowed     = false;
-        invitationsAllowed = false;
-        rocketPort         = 8222;
-        databaseUrl        = "postgresql://vaultwarden@%2Frun%2Fpostgresql/vaultwarden";
-        enableDbWal        = "false";
-      };
-    };
-    postgresql = {
-      enable = true;
-      package        = pkgs.postgresql_14;
-      authentication = lib.mkForce ''
-        # TYPE  DATABASE        USER            ADDRESS                 METHOD
-        local   all             all                                     trust
-        host    all             all             127.0.0.1/32            trust
-        host    all             all             ::1/128                 trust
-      '';
-      initialScript = pkgs.writeText "backend-initScript" ''
-        CREATE ROLE vaultwarden WITH LOGIN PASSWORD 'vaultwarden' CREATEDB;
-        CREATE DATABASE vaultwarden;
-        GRANT ALL PRIVILEGES ON DATABASE vaultwarden TO vaultwarden;
-      '';
-      ensureDatabases = ["vaultwarden"];
-      ensureUsers     = [
-       {
-         name = "vaultwarden";
-         ensurePermissions = {
-           "DATABASE vaultwarden" = "ALL PRIVILEGES";
-         };
-       }
-      ];
-    };
-    postgresqlBackup = {
-      enable    = true;
-      databases = [ "vaultwarden" ];
-    };
-    nginx = {
-      enable = true;
-      recommendedProxySettings = true;
-      recommendedGzipSettings  = true;
-      recommendedOptimisation  = true;
-      recommendedTlsSettings   = true;
-
-      virtualHosts = {
-        "${dnsBitwarden}" = {
-          forceSSL   = true;
-          enableACME = (builtins.pathExists "${joshvanlDNSPath}/acme/credentials.secret");
-          acmeRoot   = null;
-          locations."/" = {
-            proxyPass = "http://127.0.0.1:8222";
-            proxyWebsockets = true;
-          };
-          extraConfig = ''
-            proxy_read_timeout 90;
-            add_header Strict-Transport-Security "max-age=31536000; includeSubDomains" always;
-            add_header X-Content-Type-Options nosniff;
-            add_header X-XSS-Protection "1; mode=block";
-            add_header X-Frame-Options SAMEORIGIN;
-          '';
-        };
-      };
-    };
-  };
-
-  systemd = {
-    services = {
-      postgresql.after = [ "systemd-tmpfiles-setup.service" ];
-      nginx.after      = [ "systemd-tmpfiles-setup.service" ];
-      vaultwarden = {
-        wants   = [ "nginx.service" ];
-        after   = ["systemd-tmpfiles-setup.service"  "nginx.service" ];
-        bindsTo = [ "nginx.service" ];
-      };
-    };
-    tmpfiles.rules = [
-      "d  /persist/var/lib/postgresql   0755 josh wheel - -"
-      "d  /persist/var/lib/bitwarden_rs 0755 vaultwarden vaultwarden - -"
-      "d  /persist/var/lib/acme         0755 acme acme - -"
-    ];
-  };
-  fileSystems = {
-    "/var/lib/postgresql"   = { options = [ "bind" ]; device = "/persist/var/lib/postgresql";   };
-    "/var/lib/bitwarden_rs" = { options = [ "bind" ]; device = "/persist/var/lib/bitwarden_rs"; };
-    "/var/lib/acme"         = { options = [ "bind" ]; device = "/persist/var/lib/acme";         };
   };
 }
