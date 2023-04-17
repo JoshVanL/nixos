c3de55e76e7bafb0ba9b38853b8ecbebca9539e7
joshvanl <me@joshvanl.dev>
2023-04-17 15:06:16 +0100

    Update flakes. Overlay cmctl to v1.11.0, remove dapr-cli overlay in favor of upstream
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- machines/aarch64-linux/purple.nix
+++ machines/aarch64-linux/purple.nix
@@ -54,7 +54,7 @@
     };
     window-manager = {
       enable = true;
-      fontsize = 15;
+      fontsize = 23;
       xrandrArgs = "--output Virtual-1 --mode 4096x2160 --rate 120 --output Virtual-2 --off";
     };
   };
