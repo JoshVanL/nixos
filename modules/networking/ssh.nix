5df3541f6eb7fdcf9610b67e1dec7d840a2fdd15
joshvanl <me@joshvanl.dev>
2023-04-28 22:51:39 +0100

    modules/networking/ssh: persist host ingress ssh keys across boots
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- modules/networking/ssh.nix
+++ modules/networking/ssh.nix
@@ -40,10 +40,11 @@ in {
       "d /home/${config.me.base.username}/.ssh 0755 ${config.me.base.username} wheel - -"
       "d /persist/home/.ssh 0755 ${config.me.base.username} wheel - -"
       "L+ /home/${config.me.base.username}/.ssh/known_hosts - - - - /persist/home/.ssh/known_hosts"
-      "d /persist/etc/ssh 0755 root root - -"
+    ] ++ mkIf cfg.ingress.enable [
       "d /etc/ssh 0755 root root - -"
-      "L+ /etc/ssh/ssh_host_ed25519_key - - - - /persist/etc/ssh/ssh_host_ed25519_key"
-      "L+ /etc/ssh/ssh_host_ed25519_key.pub - - - - /persist/etc/ssh/ssh_host_ed25519_key.pub"
+      "d /keep/etc/ssh 0755 root root - -"
+      "L+ /etc/ssh/ssh_host_ed25519_key - - - - /keep/etc/ssh/ssh_host_ed25519_key"
+      "L+ /etc/ssh/ssh_host_ed25519_key.pub - - - - /keep/etc/ssh/ssh_host_ed25519_key.pub"
     ];
 
     users.users.${config.me.base.username}.openssh.authorizedKeys.keys = mkIf cfg.ingress.enable cfg.ingress.authorizedKeys;
