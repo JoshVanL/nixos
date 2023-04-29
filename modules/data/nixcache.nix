b71b9d88d1bb854d5997edd44c65a3ada6a2186d
joshvanl <me@joshvanl.dev>
2023-04-29 23:51:19 +0100

    modules/data/nixcache: fix behaviour. Make cache dir configurable
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- modules/data/nixcache.nix
+++ modules/data/nixcache.nix
@@ -13,6 +13,10 @@ in {
     secretKeyFile = mkOption {
       type = types.path;
     };
+    cacheDir = mkOption {
+      type = types.str;
+      default = "/keep/var/run/nginx/cache/nix";
+    };
     maxCacheSize = mkOption {
       type = types.str;
     };
@@ -23,77 +27,55 @@ in {
 
   config = mkIf cfg.enable {
     systemd.tmpfiles.rules = [
-      "d /keep/var/cache/nix 0755 ${config.me.base.username} wheel - -"
+      "d ${cfg.cacheDir} 0755 nginx nginx - -"
     ];
 
-    # Config based from
-    # https://github.com/nh2/nix-binary-cache-proxy/tree/b144bad7e95fc78ab50b2230df4920938899dab0
+    fileSystems = {
+      "/run/nginx/cache/nix" = { options = [ "bind" ]; device = "${cfg.cacheDir}"; };
+    };
+
     services.nginx = {
       enable = true;
       appendHttpConfig = ''
-        proxy_cache_path /keep/var/cache/nix levels=1:2 keys_zone=cachecache:100m max_size=${cfg.maxCacheSize} inactive=${cfg.maxCacheAge} use_temp_path=off;
-
-        # Cache only success status codes; in particular we don't want to cache 404s.
-        # See https://serverfault.com/a/690258/128321
-        map $status $cache_header {
-          200     "public";
-          302     "public";
-          default "no-cache";
-        }
+        proxy_cache_path /run/nginx/cache/nix levels=1:2 keys_zone=cache_zone:50m max_size=${cfg.maxCacheSize} inactive=${cfg.maxCacheAge};
       '';
 
-      virtualHosts = {
-        ${cfg.domain} = {
-          forceSSL = true;
-          enableACME = true;
-          acmeRoot = null;
+      virtualHosts."${cfg.domain}" = {
+        forceSSL = true;
+        enableACME = true;
+        extraConfig = ''
+          proxy_cache cache_zone;
+          proxy_cache_valid 200 ${cfg.maxCacheAge};
+          proxy_cache_use_stale error timeout invalid_header updating http_500 http_502 http_504 http_403 http_404 http_429;
+          proxy_ignore_headers X-Accel-Expires Expires Cache-Control Set-Cookie Vary;
+          proxy_cache_lock on;
+          proxy_ssl_server_name on;
+          proxy_ssl_verify on;
+          proxy_ssl_trusted_certificate /etc/ssl/certs/ca-certificates.crt;
+          set $upstream_endpoint https://cache.nixos.org;
+        '';
+        locations."/" = {
+          proxyPass = "$upstream_endpoint";
           extraConfig = ''
-            # Using a variable for the upstream endpoint to ensure that it is
-            # resolved at runtime as opposed to once when the config file is loaded
-            # and then cached forever (we don't want that):
-            # see https://tenzer.dk/nginx-with-dynamic-upstreams/
-            # This fixes errors like
-            #   nginx: [emerg] host not found in upstream "upstream.example.com"
-            # when the upstream host is not reachable for a short time when
-            # nginx is started.
-            resolver 8.8.8.8;
-            set $upstream_endpoint http://cache.nixos.org;
+            proxy_send_timeout 300ms;
+            proxy_connect_timeout 300ms;
+
+            error_page 502 504 =404 @fallback;
+
+            proxy_set_header Host $proxy_host;
           '';
-          locations."/" = {
-            root = "/var/public-nix-cache";
-            extraConfig = ''
-              add_header Strict-Transport-Security "max-age=31536000; includeSubDomains" always;
-              expires max;
-              add_header Cache-Control $cache_header always;
-              # Ask the upstream server if a file isn't available locally
-              error_page 404 = @fallback;
-            '';
-          };
-          locations."@fallback" = {
-            proxyPass = "$upstream_endpoint";
-            extraConfig = ''
-              proxy_cache cachecache;
-              proxy_cache_valid  200 302  60m;
-              expires max;
-              add_header Cache-Control $cache_header always;
-            '';
-          };
-          # We always want to copy cache.nixos.org's nix-cache-info file,
-          # and ignore our own, because `nix-push` by default generates one
-          # without `Priority` field, and thus that file by default has priority
-          # 50 (compared to cache.nixos.org's `Priority: 40`), which will make
-          # download clients prefer `cache.nixos.org` over our binary cache.
-          locations."= /nix-cache-info" = {
-            # Note: This is duplicated with the `@fallback` above,
-            # would be nicer if we could redirect to the @fallback instead.
-            proxyPass = "$upstream_endpoint";
-            extraConfig = ''
-              proxy_cache cachecache;
-              proxy_cache_valid  200 302  60m;
-              expires max;
-              add_header Cache-Control $cache_header always;
-            '';
-          };
+        };
+
+        locations."/nix-cache-info" = {
+          extraConfig = ''
+            return 200 "StoreDir: /nix/store\nWantMassQuery: 1\nPriority: 41\n";
+          '';
+        };
+
+        locations."@fallback" = {
+          extraConfig = ''
+            return 200 "404";
+          '';
         };
       };
     };
