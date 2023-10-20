e29c91d71525ad09912a9aad97df9b8a39eca15f
joshvanl <me@joshvanl.dev>
2023-10-20 15:42:42 +0100

    modules/dev/python: Add python packages
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- modules/dev/python.nix
+++ modules/dev/python.nix
@@ -3,6 +3,39 @@ with lib;
 let
   cfg = config.me.dev.python;
 
+  pythonPackages = py: py.withPackages(ps: with ps; [
+    requests
+    pydantic
+    fastapi
+    grpcio
+    httpx
+    aiohttp
+    dateutil
+    uvicorn
+    (
+      buildPythonPackage rec {
+        pname = "cloudevents";
+        version = "1.10.0";
+        src = fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-DE9yUBJnlTv3xsZROSFgKvzaAmgiAsZd6qur7AmFZzE=";
+        };
+        doCheck = false;
+      }
+    )
+    (
+      buildPythonPackage rec {
+        pname = "dapr";
+        version = "1.11.0";
+        src = fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-PpC970JBWAJXDBNmwT7N9JVhireg1PWr2AajTXg+QUE=";
+        };
+        doCheck = false;
+      }
+    )
+  ]);
+
 in {
   options.me.dev.python = {
     enable = mkEnableOption "dev.python";
@@ -10,7 +43,7 @@ in {
 
   config = mkIf cfg.enable {
     home-manager.users.${config.me.username}.home.packages = with pkgs; [
-      python3
+      (pythonPackages python3)
     ];
   };
 }
