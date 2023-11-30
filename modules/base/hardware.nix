ad291aa3df13ddd78c47545801a0b8802827309c
joshvanl <me@joshvanl.dev>
2023-11-30 13:43:16 +0000

    modules/base/boot: Remove zfsutil hardware option
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- modules/base/hardware.nix
+++ modules/base/hardware.nix
@@ -7,8 +7,6 @@ with lib;
       "/" = {
         device = "rpool/local/root";
         fsType = "zfs";
-        neededForBoot = true;
-        options = [ "zfsutil" ];
       };
 
       "/boot" = {
@@ -19,22 +17,18 @@ with lib;
       "/nix" = {
         device = "rpool/local/nix";
         fsType = "zfs";
-        neededForBoot = true;
-        options = [ "zfsutil" ];
       };
 
       "/keep" = {
         device = "rpool/local/keep";
         fsType = "zfs";
         neededForBoot = true;
-        options = [ "zfsutil" ];
       };
 
       "/persist" = {
         device = "rpool/safe/persist";
         fsType = "zfs";
         neededForBoot = true;
-        options = [ "zfsutil" ];
       };
     };
 
