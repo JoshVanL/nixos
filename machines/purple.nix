ddbc6ce88a8a5419a5c48097b5cdf83a0494e536
joshvanl <me@joshvanl.dev>
2023-12-07 15:54:44 +0000

    machines/purple: add arrow key maps for 60%
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- machines/purple.nix
+++ machines/purple.nix
@@ -27,6 +27,7 @@ with lib;
       enable = true;
       fontsize = 15;
       xrandrArgs = "--output Virtual-1 --mode 4096x2160 --rate 120 --output Virtual-2 --off";
+      arrowKeysMap = true;
     };
   };
 
@@ -42,6 +43,7 @@ with lib;
       inheritParentConfig = true;
       configuration = {
         me.window-manager.fontsize = mkForce 24;
+        me.window-manager.arrowKeysMap = mkForce false;
       };
     };
     onthemove-vpn-none = {
@@ -50,6 +52,7 @@ with lib;
         me.window-manager.fontsize = mkForce 24;
         me.networking.tailscale.vpn.enable = mkForce false;
         me.networking.wireguard.enable = mkForce false;
+        me.window-manager.arrowKeysMap = mkForce false;
       };
     };
   };
