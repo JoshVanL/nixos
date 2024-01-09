343146ae134aa7aebaf35444d9a3a2cdc3ef7e0f
joshvanl <me@joshvanl.dev>
2024-01-09 00:54:27 +0000

    pkgs/specialisation/completion.zsh: fix (again)
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- pkgs/specialisation/completion.zsh
+++ pkgs/specialisation/completion.zsh
@@ -1,18 +1,15 @@
-_specialisation() {
-  _specs() {
-    local -a specs
-    specs=( main /nix/var/nix/profiles/system/specialisation/*(N:t) )
+#compdef _specialisation specialisation
 
-    _wanted specs expl 'specialisation' compadd -a specs
-  }
-
-  local -a args=(
-    '(-)-q[Print current specialisation]'
-    '(-)-h[Show help]'
+function _specialisation() {
+  _arguments -C \
+    "(-)-q[Print current specialisation]" \
+    "(-)-h[Show help]" \
     "1:spec:_specs"
-  )
-
-  _arguments $args
 }
 
-compdef _specialisation specialisation
+function _specs() {
+  local -a specs
+  specs=( main /nix/var/nix/profiles/system/specialisation/*(N:t) )
+
+  _wanted specs expl 'specialisation' compadd -a specs
+}
