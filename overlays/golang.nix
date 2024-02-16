e613efa13e3e6b6a18f77f505cd44c9554db0c82
joshvanl <me@joshvanl.dev>
2024-02-16 02:14:19 +0000

    overlays/golang: pin golangci-lint to 1.55.2
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- overlays/golang.nix
+++ overlays/golang.nix
@@ -3,4 +3,28 @@ final: prev: rec {
 
   # `gocode` is fully deprecated, so replace it with a benign package.
   gocode = go;
+
+  # pin golangci-lint to 1.55.2 for Dapr.
+  golangci-lint = let
+    version = "1.55.2";
+    src = prev.fetchFromGitHub {
+      owner = "golangci";
+      repo = "golangci-lint";
+      rev = "v${version}";
+      sha256 = "sha256-DO71wfDmCuziEcsme1g1uNIl3MswA+EkQcYzOYHbG+I=";
+    };
+  in prev.golangci-lint.override rec {
+    buildGoModule = args: prev.buildGoModule ( args // {
+      inherit src version;
+      vendorHash = "sha256-0+jImfMdVocOczGWeO03YXUg5yKYTu3WeJaokSlcYFM=";
+      ldflags = [
+        "-s"
+        "-w"
+        "-X main.version=${version}"
+        "-X main.commit=v${version}"
+        "-X main.date=19700101-00:00:00"
+      ];
+    });
+  };
+
 }
