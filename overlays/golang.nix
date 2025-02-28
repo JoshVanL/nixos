e666056f70d684adc9e95490bc5ef4f3cf6111b0
joshvanl <me@joshvanl.dev>
2025-02-28 18:40:36 -0300

    overlays/golang.nix: Remove golangci-lint overlay
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- overlays/golang.nix
+++ overlays/golang.nix
@@ -3,25 +3,25 @@ final: prev: rec {
   #go = prev.go_1_22;
 
   # pin golangci-lint to 1.61.0 for Dapr.
-  golangci-lint = let
-    version = "1.61.0";
-    src = prev.fetchFromGitHub {
-      owner = "golangci";
-      repo = "golangci-lint";
-      rev = "v${version}";
-      sha256 = "sha256-2YzVNOdasal27R92l6eVdeS81mAp0ZU6kYsC/Jfvkcg=";
-    };
-  in prev.golangci-lint.override rec {
-      buildGoModule = args: prev.buildGoModule ( args // {
-        inherit src version;
-        vendorHash = "sha256-mFDCRxbLq08yRd0ko3CCPJD2BZiCB0Gwd1g+/1oR6w8=";
-        ldflags = [
-          "-s"
-          "-w"
-          "-X main.version=${version}"
-          "-X main.commit=v${version}"
-          "-X main.date=19700101-00:00:00"
-        ];
-      });
-  };
+  #golangci-lint = let
+  #  version = "1.61.0";
+  #  src = prev.fetchFromGitHub {
+  #    owner = "golangci";
+  #    repo = "golangci-lint";
+  #    rev = "v${version}";
+  #    sha256 = "sha256-2YzVNOdasal27R92l6eVdeS81mAp0ZU6kYsC/Jfvkcg=";
+  #  };
+  #in prev.golangci-lint.override rec {
+  #    buildGoModule = args: prev.buildGoModule ( args // {
+  #      inherit src version;
+  #      vendorHash = "sha256-mFDCRxbLq08yRd0ko3CCPJD2BZiCB0Gwd1g+/1oR6w8=";
+  #      ldflags = [
+  #        "-s"
+  #        "-w"
+  #        "-X main.version=${version}"
+  #        "-X main.commit=v${version}"
+  #        "-X main.date=19700101-00:00:00"
+  #      ];
+  #    });
+  #};
 }
