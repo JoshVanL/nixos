1a0c078967c66cbbf334250cf68546688078b3bd
joshvanl <me@joshvanl.dev>
2025-03-11 23:52:31 -0300

    overlays/golang: remove overlays
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- overlays/golang.nix
+++ overlays/golang.nix
@@ -1,5 +1,6 @@
 final: prev: rec {
-  go = prev.go_1_23;
+  #go = prev.go_1_24;
+  #go = prev.go_1_23;
   #go = prev.go_1_22;
 
   # pin golangci-lint to 1.61.0 for Dapr.
@@ -12,7 +13,7 @@ final: prev: rec {
   #    sha256 = "sha256-2YzVNOdasal27R92l6eVdeS81mAp0ZU6kYsC/Jfvkcg=";
   #  };
   #in prev.golangci-lint.override rec {
-  #    buildGoModule = args: prev.buildGoModule ( args // {
+  #    buildGo123Module = args: prev.buildGo124Module ( args // {
   #      inherit src version;
   #      vendorHash = "sha256-mFDCRxbLq08yRd0ko3CCPJD2BZiCB0Gwd1g+/1oR6w8=";
   #      ldflags = [
