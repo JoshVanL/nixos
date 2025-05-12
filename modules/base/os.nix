cc47cb6fa892c5e48e870ad0870f7453ef831102
joshvanl <me@joshvanl.dev>
2025-05-12 06:52:19 -0400

    modules/dev/os: set timezone to London
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- modules/base/os.nix
+++ modules/base/os.nix
@@ -2,10 +2,10 @@
 
 {
   # Set your time zone.
-  #time.timeZone = "Europe/London";
+  time.timeZone = "Europe/London";
   #time.timeZone = "America/Los_Angeles";
   #time.timeZone = "America/Buenos_Aires";
-  time.timeZone = "America/Toronto";
+  #time.timeZone = "America/Toronto";
 
   # Users.
   users = {
