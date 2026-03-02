878de036fd7bad070c893538e3f8e8eb105ac8ef
joshvanl <me@joshvanl.dev>
2026-03-02 12:14:08 +0000

    modules/dev/ai.nix: Adds gemini
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- modules/dev/ai.nix
+++ modules/dev/ai.nix
@@ -12,7 +12,9 @@ in {
     systemd.tmpfiles.rules = [
       "d /persist/home/.config/chatgpt 0755 ${config.me.username} wheel - -"
       "L+ /home/${config.me.username}/.config/chatgpt - - - - /persist/home/.config/chatgpt"
+      "L+ /home/${config.me.username}/.claude.json - - - - /keep/home/.claude.json"
       "L+ /home/${config.me.username}/.claude - - - - /keep/home/.claude"
+      "L+ /home/${config.me.username}/.gemini - - - - /keep/home/.gemini"
     ];
 
     me.nixpkgs.allowedUnfree = [ "claude-code" ];
@@ -23,6 +25,7 @@ in {
         packages = with pkgs; [
           chatgpt-cli
           claude-code
+          gemini-cli
         ];
       };
     };
