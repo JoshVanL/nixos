1158a695e4a8b82411b5f32f78fd71d0a0d8c94f
joshvanl <me@joshvanl.dev>
2026-03-02 12:13:41 +0000

    overlays/dapr.nix: Set dapr-cli v1.17.0
    Signed-off-by: joshvanl <me@joshvanl.dev>

---
+++ overlays/dapr.nix
@@ -0,0 +1,12 @@
+final: prev: rec {
+   dapr-cli = prev.dapr-cli.overrideAttrs (oldAttrs: rec {
+    version = "1.17.0";
+    src = prev.fetchFromGitHub {
+      owner = "dapr";
+      repo = "cli";
+      rev = "v${version}";
+      sha256 = "sha256-3nD6IVXUjLXpTz4OTNEEmsu7cDmMepLwguqI9nRQmxA=";
+    };
+    vendorHash = "sha256-o8lEcTTIASvhpRJveo0UciGhwSu+5z9+jQcII9+D5Z8=";
+  });
+}
