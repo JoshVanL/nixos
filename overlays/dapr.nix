59c18a327d4dd9e3c1087f3e5909288861b239ca
joshvanl <me@joshvanl.dev>
2026-03-02 12:39:35 +0000

    overlay/dapr.nix: fix ld flags
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- overlays/dapr.nix
+++ overlays/dapr.nix
@@ -8,5 +8,12 @@ final: prev: rec {
       sha256 = "sha256-3nD6IVXUjLXpTz4OTNEEmsu7cDmMepLwguqI9nRQmxA=";
     };
     vendorHash = "sha256-o8lEcTTIASvhpRJveo0UciGhwSu+5z9+jQcII9+D5Z8=";
+
+    ldflags = [
+      "-X main.version=${version}"
+      "-X main.apiVersion=1.0"
+      "-X github.com/dapr/cli/pkg/standalone.gitcommit=${src.rev}"
+      "-X github.com/dapr/cli/pkg/standalone.gitversion=${version}"
+    ];
   });
 }
