e300246f8bfe069f507e4c7f0a6b35a73042e4c1
joshvanl <me@joshvanl.dev>
2026-03-05 11:29:38 +0000

    overlays/golang.nix: build golangci-lint with go 1.26
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,11 +1,14 @@
-{ lib }:
+{ lib, inputs }:
 with lib;
 let
   overlays = mapAttrs (_: file: import ./${file}) (nixFilesNoDefault ./.);
 
+  # golang overlay needs nixpkgs-unstable for go_1_26
+  golangOverlay = import ./golang.nix { inherit inputs; };
+
 in {
   modules = {
-    nixpkgs.overlays = attrValues overlays;
+    nixpkgs.overlays = (attrValues (removeAttrs overlays ["golang"])) ++ [ golangOverlay ];
   };
-  inherit overlays;
+  overlays = overlays // { golang = golangOverlay; };
 }
--- overlays/golang.nix
+++ overlays/golang.nix
@@ -1,42 +1,36 @@
-final: prev: rec {
+{ inputs }:
+final: prev:
+let
+  unstable = import inputs.nixpkgs-unstable { inherit (prev) system; };
+in
+{
   #go = prev.go_1_24;
   #go = prev.go_1_23;
   #go = prev.go_1_22;
 
-  # pin golangci-lint to 1.61.0 for Dapr.
-  #golangci-lint = let
-  #  version = "1.64.6";
-  #  src = prev.fetchFromGitHub {
-  #    owner = "golangci";
-  #    repo = "golangci-lint";
-  #    rev = "v${version}";
-  #    sha256 = "sha256-2YzVNOdasal27R92l6eVdeS81mAp0ZU6kYsC/Jfvkca=";
-  #  };
-  #in prev.golangci-lint.override rec {
-  #    buildGo124Module = args: prev.buildGo124Module ( args // {
-  #      inherit src version;
-  #      vendorHash = "sha256-mFDCRxbLq08yRd0ko3CCPJD2BZiCB0Gwd1g+/1oR6wa=";
-  #      ldflags = [
-  #        "-s"
-  #        "-w"
-  #        "-X main.version=${version}"
-  #        "-X main.commit=v${version}"
-  #        "-X main.date=19700101-00:00:00"
-  #      ];
-  #    });
-  #};
-
-   golangci-lint = prev.golangci-lint.overrideAttrs (oldAttrs: rec {
-    #version = "2.8.0";
-    version = "1.64.4";
-    src = prev.fetchFromGitHub {
-      owner = "golangci";
-      repo = "golangci-lint";
-      rev = "v${version}";
-      sha256 = "sha256-BrkBIf4WP3COAac/5vre8fHLgDneg5Gm31nNq8sXzEE=";
-      #sha256 = "sha256-w6MAOirj8rPHYbKrW4gJeemXCS64fNtteV6IioqIQTQ=";
-    };
-    #vendorHash = "sha256-/Vqo/yrmGh6XipELQ9NDtlMEO2a654XykmvnMs0BdrI=";
-    vendorHash = "sha256-xUKse9yTAVuysmPwmX4EXdlpg6NYKfT5QB1RgmBQvhk=";
-  });
+  golangci-lint = prev.golangci-lint.override {
+    buildGo125Module = fnOrAttrs:
+      let
+        wrapFn = finalAttrs:
+          let
+            orig = if builtins.isFunction fnOrAttrs then fnOrAttrs finalAttrs else fnOrAttrs;
+          in orig // rec {
+            version = "2.10.1";
+            src = prev.fetchFromGitHub {
+              owner = "golangci";
+              repo = "golangci-lint";
+              rev = "v${version}";
+              sha256 = "sha256-rHttQ+QJ9JrFvgfoX68Y0lD6BUv/aoOpRRFvZ1BIGIs=";
+            };
+            vendorHash = "sha256-yREpROQJ300+mii7R2oiyDjOGcYXBpv3o/park0TJYE=";
+            ldflags = [
+              "-s"
+              "-w"
+              "-X main.version=${version}"
+              "-X main.commit=v${version}"
+              "-X main.date=19700101-00:00:00"
+            ];
+          };
+      in unstable.buildGo126Module wrapFn;
+  };
 }
