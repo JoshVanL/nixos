e07e26b40fe89c4f5bf89d663dc355e3c703844c
joshvanl <me@joshvanl.dev>
2026-03-06 17:06:35 +0000

    overlays/dapr.nix: update to 1.17.1-rc.1
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- overlays/dapr.nix
+++ overlays/dapr.nix
@@ -1,11 +1,11 @@
 final: prev: rec {
    dapr-cli = prev.dapr-cli.overrideAttrs (oldAttrs: rec {
-    version = "1.17.0";
+    version = "1.17.1-rc.1";
     src = prev.fetchFromGitHub {
       owner = "dapr";
       repo = "cli";
       rev = "v${version}";
-      sha256 = "sha256-3nD6IVXUjLXpTz4OTNEEmsu7cDmMepLwguqI9nRQmxA=";
+      sha256 = "sha256-Y+b4cXI1mLQ6YB3JlSIkvjNz2arOBZfv6EMJHHEqU0w=";
     };
     vendorHash = "sha256-o8lEcTTIASvhpRJveo0UciGhwSu+5z9+jQcII9+D5Z8=";
 
