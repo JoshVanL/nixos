ea599602f1c579af54d9c082244cbac73b4b6fd3
joshvanl <me@joshvanl.dev>
2026-03-10 16:40:10 +0000

    overlays/claude.nix: update to 2.1.72
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- overlays/claude-code.nix
+++ overlays/claude-code.nix
@@ -1,11 +1,11 @@
 final: prev: {
   claude-code = prev.stdenv.mkDerivation rec {
     pname = "claude-code";
-    version = "2.1.63";
+    version = "2.1.72";
 
     src = prev.fetchzip {
       url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-      hash = "sha256-tVk1GXqh9Ice8ZbbLnmN4sSlIY41KsrqWi2eDo47/zI=";
+      hash = "sha256-sSIjVW0DWb/rTv0AN7ineU4h45jnzFfElqkr1/+wWAk=";
     };
 
     nativeBuildInputs = [ prev.makeWrapper ];
@@ -19,6 +19,7 @@ final: prev: {
       makeWrapper ${prev.nodejs}/bin/node $out/bin/claude \
         --add-flags "$out/lib/node_modules/@anthropic-ai/claude-code/cli.js" \
         --set DISABLE_AUTOUPDATER 1 \
+        --set DISABLE_AUTO_MIGRATE_TO_NATIVE 1 \
         --unset DEV
 
       runHook postInstall
