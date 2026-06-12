d60861dc288e55783b818d54d12b6d974f45f218
joshvanl <me@joshvanl.dev>
2026-06-12 12:50:44 +0100

    pkgs/diagrid-cli: update 1.39.0
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- pkgs/diagrid-cli/default.nix
+++ pkgs/diagrid-cli/default.nix
@@ -5,11 +5,11 @@
 }:
 
 let
-  version = "0.634.0";
+  version = "1.39.0";
   bucket = "bkt-p-cli-common-us-central1-95640";
   sysMap = {
-    x86_64-linux = { os = "linux"; arch = "amd64"; hash = "sha256-fwYI661Kb2GTe41rUjdqN1l+/SLiXNLKrAQZjZ+yWl8="; };
-    aarch64-linux = { os = "linux"; arch = "arm64"; hash = "sha256-BhBLg2/nZuJDvko1a7XlCXX47+XYSkY9pCMfsSYbxbo="; };
+    x86_64-linux = { os = "linux"; arch = "amd64"; hash = "sha256-BaRT2LmLfnY9bwtZRRbgEuNEGOdRbeDcDGUjClNwnls="; };
+    aarch64-linux = { os = "linux"; arch = "arm64"; hash = "sha256-3McvPD+AEF2OBYvLqOyVzn1qx0eV+jMvuoap5UBSqdQ="; };
   };
   plat = sysMap.${stdenv.hostPlatform.system};
   name = "diagrid_${plat.os}_${plat.arch}";
